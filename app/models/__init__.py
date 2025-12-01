0263a7e1208e167d4f27555607691b1c7431fe74
LotusBoard Admin <admin@lotusboard.local>
2025-12-01 14:31:06 +0330

    Add model imports to fix SQLAlchemy foreign key resolution

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.user import User
+from app.models.token import Token
+from app.models.guardian import Guardian
+from app.models.enclosure import Enclosure
+from app.models.animal import Animal
+from app.models.application import Application
+from app.models.favorite import Favorite
